f18369951054045f3841bd77a417092b5e6b571f
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2024-01-08 18:02:24 +0800

    [*]逻辑增加

--- walter_tools/bluetooth_tools/bleak_discovery_tool.py
+++ walter_tools/bluetooth_tools/bleak_discovery_tool.py
@@ -2,10 +2,15 @@
 import asyncio
 from bleak import BleakScanner
 
+
 async def main():
     # 发现附近设备
     devices = await BleakScanner.discover()
+    count = 0
     for d in devices:
         print(d)
+        count += 1
+    print("附近设备数:{}".format(count))
+
 
 asyncio.run(main())
